ffcef588f061ff6661d9124a11974303db673ac4
agunubu <agunubu@unomaha.edu>
2025-03-08 21:19:05 +0000

    finished with problem 10 and 4

--- Problem10.py
+++ Problem10.py
@@ -2,10 +2,13 @@
 #Project Euler problem 10
 
 from NumberTests import isPrime
-
  
 def main():
-
+    sumPrime = 0 
+    for s in range(2000000):
+       if isPrime(s):
+        sumPrime = sumPrime + s
+    print(sumPrime)
 
 
 if __name__ == '__main__':
--- Problem4.py
+++ Problem4.py
@@ -6,13 +6,16 @@ from NumberTests import isPalindrome
  
 def main():
     maxPalindrome = 0
+    factor1 = 0
+    factor2 = 0
 
     for i in range(100, 1000):  
         for j in range(100, 1000):
             product = i * j
             if isPalindrome(product) and product > maxPalindrome:
                 maxPalindrome = product
-    print(maxPalindrome)
+                factor1, factor2 = i, j
+    print((maxPalindrome), "is the product of", factor1, "*", factor2)
 
 if __name__ == '__main__':
   main()
